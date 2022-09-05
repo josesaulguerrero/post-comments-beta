f574eeaa751d0e380bd0f4cafe4aebf5587c1a04
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-05 13:28:32 -0500

    Add UpdateView use case

---
+++ src/main/java/co/com/post_comments/beta/business/usecases/UpdateViewUseCase.java
@@ -0,0 +1,19 @@
+package co.com.post_comments.beta.business.usecases;
+
+import co.com.post_comments.beta.business.commons.ViewUpdater;
+import co.com.post_comments.beta.business.gateways.EventBus;
+import co.com.sofka.domain.generic.DomainEvent;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UpdateViewUseCase {
+    private final ViewUpdater viewUpdater;
+    private final EventBus eventBus;
+
+    public void accept(DomainEvent event) {
+        this.viewUpdater.applyEvent(event);
+        this.eventBus.publishDomainEvent(event);
+    }
+}
