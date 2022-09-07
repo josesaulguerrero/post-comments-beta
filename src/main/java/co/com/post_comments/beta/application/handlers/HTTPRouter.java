b382ff313627d866e2e8dbc581728bd2e0f437f5
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-09-07 10:31:27 -0500

    Add HTTP router functions

--- src/main/java/co/com/post_comments/beta/application/handlers/HTTPRouter.java
+++ src/main/java/co/com/post_comments/beta/application/handlers/HTTPRouter.java
@@ -1,10 +1,51 @@
 package co.com.post_comments.beta.application.handlers;
 
 
+import co.com.post_comments.beta.business.commons.views.PostView;
+import co.com.post_comments.beta.business.usecases.FindAllUseCase;
+import co.com.post_comments.beta.business.usecases.FindByPostIdUseCase;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
 public class HTTPRouter {
+    @Bean
+    public RouterFunction<ServerResponse> getAllPosts(FindAllUseCase useCase) {
+        return RouterFunctions.route(
+                RequestPredicates.GET("/api/v1/posts/all"),
+                request ->
+                        ServerResponse
+                                .ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body(
+                                        BodyInserters.fromPublisher(
+                                                useCase.apply(),
+                                                PostView.class
+                                        )
+                                )
+        );
+    }
 
-    //Create a route that allows you to make a Get Http request that brings you all the posts and also a post by its id
+    @Bean
+    public RouterFunction<ServerResponse> getAllPosts(FindByPostIdUseCase useCase) {
+        return RouterFunctions.route(
+                RequestPredicates.GET("/api/v1/posts/{postId}"),
+                request ->
+                        ServerResponse
+                                .ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body(
+                                        BodyInserters.fromPublisher(
+                                                useCase.apply(request.pathVariable("postId")),
+                                                PostView.class
+                                        )
+                                )
+        );
+    }
 }
